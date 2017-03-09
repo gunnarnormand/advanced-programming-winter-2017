92cd48f8454de0e006dce63ed8464b4991872133
Gunnar Normand <gunnarnormand@yahoo.com>
2017-03-09 01:32:07 -0500

    localStorage working

--- finalProject/assets/js/main.js
+++ finalProject/assets/js/main.js
@@ -7,76 +7,75 @@ window.onload = function() {
 
 const app = (function() {
 	const $searchForm = document.querySelector('#search-form');
-	const $searchInput = document.querySelector('#search-input');
-	const $mainRow = document.querySelector('#main-row');	
-
-	const items = [];
-
-
+	const $placesList = document.querySelector('#places-list');
+		
+	// try to get from localStorage if not there fall back to empty array
+	const places = JSON.parse(localStorage.getItem('items')) || [];
 
 
+	const init = function() {	
+		
+		// const $deletedBtn = document.querySelector('[name=deleted]');
 
+		displayPlace(places, $placesList);
+		$searchForm.addEventListener('submit', addPlace);
+		$placesList.addEventListener('click', deletePlace);
 
-
-
-
-	const init = function() {
-		getPlace();
+		
 	}
 
-	const getPlace = function() {
-		$searchForm.addEventListener('submit', (e) => {
-			e.preventDefault();
-			
-			let searchInput = $searchInput.value;
-			searchInput.trim();
-
-			searchInput.length > 0 ? createPlace(searchInput) : $searchInput.focus();
-			
-			$searchInput.value = '';
-		});
+	function addPlace(e) {
+		e.preventDefault();
+		const $placesInput = this.querySelector('[name=place]');
+		const placeText = $placesInput.value;
+		placeText.trim();
+		const place = {
+			placeText,
+			deleted: false
+		}
+		if (placeText.length > 0) {
+			places.push(place);
+			displayPlace(places, $placesList);
+			localStorage.setItem('items', JSON.stringify(places));
+			this.reset();
+		} else {
+			$placesInput.focus();
+		}		
 	}
 
+	function displayPlace(places = [], placesList) {
+		placesList.innerHTML = places.map((place, i) => {
+			return `
+				<div class="col-md-8 col-lg-7 mx-auto float-xs-none white z-depth-1 py-2 px-2 animated zoomInDown">
+					<h2 class="h2-responsive"><strong>${place.placeText}</strong></h2>
+					<div class="card-block">
+		                <div class="text-center">
+		                    <button type="button" data-toggle="modal" data-target="#myModal" class="btn btn-primary waves-effect btn-block">add review</button>
+		                    <button type="button" name="deleted" data-index=${i} class="btn btn-sm btn-secondary waves-effect btn-block" >remove</button>
+		                </div>
+			        </div>
+		        </div> 
+			`;
+		}).join('');
+
+
+		console.clear();
+		console.table(places);
+	}
+
+
+	function deletePlace(e) {
+		if(!e.target.matches('[name=deleted]')) return; //skip this unless its a deleteButton
+		
+		const el = e.target;
+		const index = el.dataset.index;
+		places[index].deleted = !places[index].deleted;
+		localStorage.setItem('items', JSON.stringify(places));
+		displayPlace(places, $placesList);
+
+	}
 	
-	const createPlace = function(searchInput) {
 
-		let $newPlace = document.createElement('div');
-		$newPlace.classList.add('col-md-8', 
-								'col-lg-7', 
-								'mx-auto', 
-								'float-xs-none',
-								'white', 
-								'z-depth-1',
-								'py-2',
-								'px-2',
-								'animated',
-								'zoomInDown'
-							);
-		
-		
-		let $newPlaceName = document.createElement('h2');
-		$newPlaceName.classList.add('h2-responsive');
-		$newPlaceName.innerHTML = `<strong>${searchInput}</strong>`;
-
-		let $newPlaceButton = document.createElement('div');
-		$newPlaceButton.classList.add('card-block');
-		$newPlaceButton.innerHTML = 
-		`
-			<button type="button" data-toggle="modal" data-target="#myModal" class="btn btn-primary waves-effect btn-block">add review</button>
-			<button type="button" class="btn btn-sm btn-secondary waves-effect btn-block">remove</button>
-		`;
-
-		
-		 
-
-		
-		
-
-		$mainRow.appendChild($newPlace);
-		$newPlace.appendChild($newPlaceName);
-		$newPlace.appendChild($newPlaceButton);
-
-	}
 
 
 
--- finalProject/assets/lib/main.js
+++ finalProject/assets/lib/main.js
@@ -7,45 +7,60 @@ window.onload = function () {
 
 var app = function () {
 	var $searchForm = document.querySelector('#search-form');
-	var $searchInput = document.querySelector('#search-input');
-	var $mainRow = document.querySelector('#main-row');
+	var $placesList = document.querySelector('#places-list');
 
-	var items = [];
+	// try to get from localStorage if not there fall back to empty array
+	var places = JSON.parse(localStorage.getItem('items')) || [];
 
 	var init = function init() {
-		getPlace();
+
+		// const $deletedBtn = document.querySelector('[name=deleted]');
+
+		displayPlace(places, $placesList);
+		$searchForm.addEventListener('submit', addPlace);
+		$placesList.addEventListener('click', deletePlace);
 	};
 
-	var getPlace = function getPlace() {
-		$searchForm.addEventListener('submit', function (e) {
-			e.preventDefault();
+	function addPlace(e) {
+		e.preventDefault();
+		var $placesInput = this.querySelector('[name=place]');
+		var placeText = $placesInput.value;
+		placeText.trim();
+		var place = {
+			placeText: placeText,
+			deleted: false
+		};
+		if (placeText.length > 0) {
+			places.push(place);
+			displayPlace(places, $placesList);
+			localStorage.setItem('items', JSON.stringify(places));
+			this.reset();
+		} else {
+			$placesInput.focus();
+		}
+	}
 
-			var searchInput = $searchInput.value;
-			searchInput.trim();
+	function displayPlace() {
+		var places = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : [];
+		var placesList = arguments[1];
 
-			searchInput.length > 0 ? createPlace(searchInput) : $searchInput.focus();
+		placesList.innerHTML = places.map(function (place, i) {
+			return '\n\t\t\t\t<div class="col-md-8 col-lg-7 mx-auto float-xs-none white z-depth-1 py-2 px-2 animated zoomInDown">\n\t\t\t\t\t<h2 class="h2-responsive"><strong>' + place.placeText + '</strong></h2>\n\t\t\t\t\t<div class="card-block">\n\t\t                <div class="text-center">\n\t\t                    <button type="button" data-toggle="modal" data-target="#myModal" class="btn btn-primary waves-effect btn-block">add review</button>\n\t\t                    <button type="button" name="deleted" data-index=' + i + ' class="btn btn-sm btn-secondary waves-effect btn-block" >remove</button>\n\t\t                </div>\n\t\t\t        </div>\n\t\t        </div> \n\t\t\t';
+		}).join('');
 
-			$searchInput.value = '';
-		});
-	};
+		console.clear();
+		console.table(places);
+	}
 
-	var createPlace = function createPlace(searchInput) {
+	function deletePlace(e) {
+		if (!e.target.matches('[name=deleted]')) return; //skip this unless its a deleteButton
 
-		var $newPlace = document.createElement('div');
-		$newPlace.classList.add('col-md-8', 'col-lg-7', 'mx-auto', 'float-xs-none', 'white', 'z-depth-1', 'py-2', 'px-2', 'animated', 'zoomInDown');
-
-		var $newPlaceName = document.createElement('h2');
-		$newPlaceName.classList.add('h2-responsive');
-		$newPlaceName.innerHTML = '<strong>' + searchInput + '</strong>';
-
-		var $newPlaceButton = document.createElement('div');
-		$newPlaceButton.classList.add('card-block');
-		$newPlaceButton.innerHTML = '\n\t\t\t<button type="button" data-toggle="modal" data-target="#myModal" class="btn btn-primary waves-effect btn-block">add review</button>\n\t\t\t<button type="button" class="btn btn-sm btn-secondary waves-effect btn-block">remove</button>\n\t\t';
-
-		$mainRow.appendChild($newPlace);
-		$newPlace.appendChild($newPlaceName);
-		$newPlace.appendChild($newPlaceButton);
-	};
+		var el = e.target;
+		var index = el.dataset.index;
+		places[index].deleted = !places[index].deleted;
+		localStorage.setItem('items', JSON.stringify(places));
+		displayPlace(places, $placesList);
+	}
 
 	return {
 		init: init
